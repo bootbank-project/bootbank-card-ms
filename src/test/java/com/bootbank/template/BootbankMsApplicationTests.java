85aa462260a5b1d528060189c8a88fdd624258a8
Kochari <kochari.aghalarov@gmail.com>
2026-07-24 03:00:35 +0400

    rename entities

---
+++ src/test/java/com/bootbank/template/BootbankMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootbank.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootbankMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
